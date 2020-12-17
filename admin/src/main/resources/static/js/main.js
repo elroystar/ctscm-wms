41188eb5711b90342704ab526b8fca1710196c22
chenziyi@sogou-inc.com <PA$$w0rd>
2020-12-17 17:19:16 +0800

    库区select选择

--- admin/src/main/resources/static/js/main.js
+++ admin/src/main/resources/static/js/main.js
@@ -409,3 +409,14 @@ layui.use(['element', 'form', 'layer', 'upload'], function () {
         $(this).parent('.upload-item').remove();
     });
 });
+
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
